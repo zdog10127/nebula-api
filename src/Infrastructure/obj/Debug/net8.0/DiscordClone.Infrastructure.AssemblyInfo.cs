d25d50461ed34c17dcc1ffbe41c640f0d35143ab
Gabriel Teles <gabrieltelessantos48@gmail.com>
2026-08-22 09:03:28 -0300

    fix: :sparkles:

--- src/Infrastructure/obj/Debug/net8.0/DiscordClone.Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net8.0/DiscordClone.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscordClone.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5580196ea99ad6ce8b1bf49c98fa074d82362079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0879d8e2d8ae5de046a7772f5c0adf42e99df4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscordClone.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscordClone.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
